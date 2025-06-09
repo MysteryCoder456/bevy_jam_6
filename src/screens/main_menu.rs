a13ed48dc3421583a594f7a69fdcb3521ce12d7a
MysteryCoder456 <rehatbir.singh@gmail.com>
2025-06-09 19:36:30 +0400

    feat: main menu interactivity

--- src/screens/main_menu.rs
+++ src/screens/main_menu.rs
@@ -40,21 +40,19 @@ fn button(label: impl Into<String>, font: Handle<Font>, size: f32) -> impl Bundl
 }
 
 fn spawn_main_menu(mut commands: Commands, assets: Res<GameAssets>) {
-    commands
-        .spawn((
-            Name::new("Main Menu UI"),
-            MainMenuUI,
-            Node {
-                height: Val::Vh(100.0),
-                display: Display::Flex,
-                flex_direction: FlexDirection::Column,
-                justify_self: JustifySelf::Center,
-                justify_content: JustifyContent::Center,
-                ..Default::default()
-            },
-        ))
-        .with_children(|parent| {
-            parent.spawn((
+    commands.spawn((
+        Name::new("Main Menu UI"),
+        MainMenuUI,
+        Node {
+            width: Val::Vw(100.0),
+            height: Val::Vh(100.0),
+            flex_direction: FlexDirection::Column,
+            justify_content: JustifyContent::Center,
+            align_items: AlignItems::Center,
+            ..Default::default()
+        },
+        children![
+            (
                 Name::new("Title"),
                 Text::new("Toilet Paper"),
                 TextColor(WHITE.into()),
@@ -67,11 +65,22 @@ fn spawn_main_menu(mut commands: Commands, assets: Res<GameAssets>) {
                     justify: JustifyText::Center,
                     ..Default::default()
                 },
-            ));
-
-            parent.spawn((PlayButton, button("Play", assets.ui_font.clone(), 32.0)));
-            parent.spawn((QuitButton, button("Quit", assets.ui_font.clone(), 32.0)));
-        });
+            ),
+            (
+                Name::new("Button Container"),
+                Node {
+                    flex_direction: FlexDirection::Column,
+                    padding: UiRect::vertical(Val::Px(32.0)),
+                    row_gap: Val::Px(12.0),
+                    ..Default::default()
+                },
+                children![
+                    (PlayButton, button("Play", assets.ui_font.clone(), 32.0)),
+                    (QuitButton, button("Quit", assets.ui_font.clone(), 32.0))
+                ]
+            )
+        ],
+    ));
 }
 
 fn despawn_main_menu(mut commands: Commands, query: Single<Entity, With<MainMenuUI>>) {
@@ -80,28 +89,36 @@ fn despawn_main_menu(mut commands: Commands, query: Single<Entity, With<MainMenu
 
 fn play_button_interaction(
     mut next_screen: ResMut<NextState<Screen>>,
-    query: Single<&Interaction, (Changed<Interaction>, With<PlayButton>)>,
+    mut query: Single<(&Interaction, &mut TextColor), (Changed<Interaction>, With<PlayButton>)>,
 ) {
-    match *query {
+    match *query.0 {
         Interaction::Pressed => {
             log::info!("Starting game...");
             next_screen.set(Screen::Level);
         }
-        Interaction::Hovered => {}
-        Interaction::None => {}
+        Interaction::Hovered => {
+            *query.1 = SLATE_GRAY.into();
+        }
+        Interaction::None => {
+            *query.1 = WHITE.into();
+        }
     }
 }
 
 fn quit_button_interaction(
     mut events: EventWriter<AppExit>,
-    query: Single<&Interaction, (Changed<Interaction>, With<QuitButton>)>,
+    mut query: Single<(&Interaction, &mut TextColor), (Changed<Interaction>, With<QuitButton>)>,
 ) {
-    match *query {
+    match *query.0 {
         Interaction::Pressed => {
             log::info!("Exiting app...");
             events.write(AppExit::Success);
         }
-        Interaction::Hovered => {}
-        Interaction::None => {}
+        Interaction::Hovered => {
+            *query.1 = SLATE_GRAY.into();
+        }
+        Interaction::None => {
+            *query.1 = WHITE.into();
+        }
     }
 }
