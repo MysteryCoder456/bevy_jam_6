6b3df3bf90185e902c0b7cd49527b8e99d67583a
MysteryCoder456 <rehatbir.singh@gmail.com>
2025-06-11 08:33:47 +0400

    feat: debug shelf spawning

--- src/screens/level.rs
+++ src/screens/level.rs
@@ -7,8 +7,24 @@ use std::{collections::HashMap, fmt::Display};
 use crate::screens::Screen;
 use avian2d::prelude::*;
 use bevy::prelude::*;
+use bevy_enhanced_input::prelude::*;
+use player::Player;
 use shelf::{ShelfOrientation, SpawnShelf};
 use shopper::SpawnShopper;
+
+#[cfg(feature = "dev")]
+#[derive(InputContext)]
+struct DebugLevelContext;
+
+#[cfg(feature = "dev")]
+#[derive(Debug, InputAction)]
+#[input_action(output = bool)]
+struct DebugSpawnShelfVertical;
+
+#[cfg(feature = "dev")]
+#[derive(Debug, InputAction)]
+#[input_action(output = bool)]
+struct DebugSpawnShelfHorizontal;
 
 #[derive(PhysicsLayer, Default)]
 enum GameLayer {
@@ -48,11 +64,34 @@ pub fn plugin(app: &mut App) {
     app.register_type::<Item>();
     app.register_type::<Inventory>();
 
+    // Add debug input actions
+    cfg_if::cfg_if! {
+        if #[cfg(feature = "dev")] {
+            let mut actions = Actions::<DebugLevelContext>::default();
+            actions
+                .bind::<DebugSpawnShelfVertical>()
+                .to(KeyCode::KeyV)
+                .with_conditions(Press::default());
+            actions
+                .bind::<DebugSpawnShelfHorizontal>()
+                .to(KeyCode::KeyH)
+                .with_conditions(Press::default());
+        }
+    }
+
     // Add game element plugins
     app.add_plugins((player::plugin, shelf::plugin, shopper::plugin));
 
     // Gameplay systems
     app.add_systems(OnEnter(Screen::Level), spawn_level);
+
+    // Add debug systems
+    cfg_if::cfg_if! {
+        if #[cfg(feature = "dev")] {
+            app.add_observer(spawn_vertical_shelf);
+            app.add_observer(spawn_horizontal_shelf);
+        }
+    }
 }
 
 fn spawn_level(
@@ -87,4 +126,30 @@ fn spawn_level(
             position: Vec2::new(-100.0, -100.0),
         },
     ]);
+}
+
+fn spawn_vertical_shelf(
+    _trigger: Trigger<Fired<DebugSpawnShelfVertical>>,
+    mut shelf_events: EventWriter<SpawnShelf>,
+    player_query: Single<&Transform, With<Player>>,
+) {
+    let offset = Vec2::new(0.0, 50.0);
+    shelf_events.write(SpawnShelf {
+        position: player_query.translation.truncate() + offset,
+        orientation: ShelfOrientation::Vertical,
+        main_item: Item::ToiletPaper,
+    });
+}
+
+fn spawn_horizontal_shelf(
+    _trigger: Trigger<Fired<DebugSpawnShelfHorizontal>>,
+    mut shelf_events: EventWriter<SpawnShelf>,
+    player_query: Single<&Transform, With<Player>>,
+) {
+    let offset = Vec2::new(50.0, 0.0);
+    shelf_events.write(SpawnShelf {
+        position: player_query.translation.truncate() + offset,
+        orientation: ShelfOrientation::Horizontal,
+        main_item: Item::ToiletPaper,
+    });
 }
